9d82e86d04388c942fbe3cb6433094705b0688e6
Esharpp <e.boumhah@hotmail.com>
2026-05-19 16:51:51 +0100

    Learned how to write classes and call constructors

---
+++ 01-oop/call_constructor.py
@@ -0,0 +1,8 @@
+class Rectangle:
+    def __init__(self, length, width, color="red"):
+        self.length = length
+        self.width = width
+        self.color = color
+
+# color="red" will set the colour of the class to red if no colour info is provided
+        
